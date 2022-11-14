695781c5f076b5717d3883dd549d1c9e83e68368
fatemeRasolzade <fatemerasolzade.1424@gmail.com>
2022-11-14 11:32:51 +0330

    learning event props

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import "./App.css";
+import Button from "./components/Button";
 import Greet from "./components/Greet";
 import Heading from "./components/Heading";
+import Input from "./components/Input";
 import Oscar from "./components/Oscar";
 import Person from "./components/Person";
 import PersonList from "./components/PersonList";
@@ -29,6 +31,13 @@ function App() {
       <Oscar>
         <Heading>Oscar goes to leonardo dicpario!</Heading>
       </Oscar>
+      <Button
+        handleClick={(event, id) => {
+          console.log(event, id);
+          alert("hello world!");
+        }}
+      />
+      <Input value="" handleChange={(event) => console.log(event.target.value)} />
     </div>
   );
 }

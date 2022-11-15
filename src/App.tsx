cb0176d6d669003827e0ad99a61a012323b58463
fatemeRasolzade <fatemerasolzade.1424@gmail.com>
2022-11-15 13:50:21 +0330

    learning useReducer and useContext hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import "./App.css";
 import Button from "./components/Button";
 import Container from "./components/Container";
+import Box from "./components/context/Box";
+import { ThemeContextProvider } from "./components/context/ThemeContext";
 import Greet from "./components/Greet";
 import Heading from "./components/Heading";
 import Input from "./components/Input";
 import Oscar from "./components/Oscar";
 import Person from "./components/Person";
 import PersonList from "./components/PersonList";
+import Counter from "./components/state/Counter";
 import LoggedIn from "./components/state/LoggedIn";
 import User from "./components/state/User";
 import Status from "./components/Status";
@@ -54,6 +57,11 @@ function App() {
       <hr />
       <User />
       <hr />
+      <Counter />
+      <hr />
+      <ThemeContextProvider>
+        <Box />
+      </ThemeContextProvider>
     </div>
   );
 }
